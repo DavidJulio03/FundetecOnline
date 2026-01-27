92bba3c635cb388b55b2e299ac88f40b909006ab
DavidJulio03 <246344722+DavidJulio03@users.noreply.github.com>
2026-01-27 13:11:37 -0500

    build: :heavy_plus_sign: EmailJS

--- src/pages/Home/hooks/useContactForm.jsx
+++ src/pages/Home/hooks/useContactForm.jsx
@@ -1,40 +1,39 @@
-import { useState } from 'react';
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
 
-export const useContactForm = (targetEmail) => {
-  const [status, setStatus] = useState('idle'); // 'idle' | 'loading' | 'success' | 'error'
+export const useContactForm = () => {
+  const [status, setStatus] = useState("idle"); 
+  // 'idle' | 'loading' | 'success' | 'error'
 
   const sendForm = async (formData) => {
-    setStatus('loading');
+    setStatus("loading");
 
     try {
       const data = Object.fromEntries(formData.entries());
 
-      const response = await fetch(`https://formsubmit.co/ajax/${targetEmail}`, {
-        method: "POST",
-        headers: { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            ...data,
-            _subject: "Nuevo Lead desde la Web (Inscripción)",
-            _template: "table",
-            _captcha: "false"
-        })
-      });
+      const payload = {
+        name: "Nuevo Lead desde la Web", // título
+        time: new Date().toLocaleString(), // fecha/hora
+        message: data.nombre,
+        number: data.telefono,
+        interes: data.programa_interes,
+      };
 
-      if (response.ok) {
-        setStatus('success');
-        return true;
-      } else {
-        throw new Error('Error en el envío');
-      }
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        payload,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
+
+      setStatus("success");
+      return true;
     } catch (error) {
-      console.error(error);
-      setStatus('error');
+      console.error("EmailJS Error:", error);
+      setStatus("error");
       return false;
     } finally {
-      setTimeout(() => setStatus('idle'), 3000);
+      setTimeout(() => setStatus("idle"), 3000);
     }
   };
 
